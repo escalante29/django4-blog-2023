449b17fd4506d42514a9374b2ba0c45d91652b58
cescalante-dev <carlos.dev.escalante@gmail.com>
2023-03-01 18:07:56 -0600

    Django 4 by Example - Add Postgres SearchVector and Ranked search

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 class PostListView(ListView):
     """
@@ -136,9 +136,16 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            # search_vector = SearchVector('title', 'body', config='spanish')
+            # search_query = SearchQuery(query, config='spanish')
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')    
+
+            # search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search = SearchVector('title', 'body'),
-            ).filter(search=query)
+                search = search_vector,
+                rank = SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
     
     return render(request,
                   'blog/post/search.html',
